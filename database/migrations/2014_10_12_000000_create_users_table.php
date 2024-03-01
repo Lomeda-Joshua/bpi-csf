d6ecd40406808686116d3ec2ec5a98f8b64afff7
josh-lomeda-ics-bpi <joshualomeda.e14@gmail.com>
2024-03-01 15:08:00 +0800

    Build: Added columns to users table, created the bpi-csf table, and migrated the table to database; Enhancement: Installed and published laravel breeze authentication system

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->nullable();
             $table->string('password');
+            $table->tinyInteger('role_id')->nullable();
+            $table->tinyInteger('office_id')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
